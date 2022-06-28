ddb3173acbfe63b546406f832d3e130a564329e1
leehyeonho <dlgusgh0812@gmail.com>
2022-06-28 15:22:40 +0900

    #1.6 Chat Completed

--- src/server.js
+++ src/server.js
@@ -8,20 +8,26 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.render("/"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-// app.listen(3000, handleListen);
 
-// function handleConnection(socket) {
-//     console.log(socket);
-// }
-// wss.on("connection", handleConnection);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function onSocketClose() {
+    console.log("Disconnected from the Browser ❌");
+}
+
+const sockets = [];
 
 wss.on("connection", (socket) => {
+    sockets.push(socket);
     console.log("Connected to Browser ✅");
-    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-    socket.send("hello!!");
+    socket.on("close", onSocketClose);
+    socket.on("message", (message) => {
+        sockets.forEach((aSocket) => aSocket.send(message.toString()));
+    });
 });
 
 server.listen(3000, handleListen);
